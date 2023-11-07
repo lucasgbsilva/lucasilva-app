a8ab34f4f88c51ab240eec4a0737145dc2f2dfcd
lucasgbsilva <lucas.outro0106@gmail.com>
2023-11-07 14:36:04 -0300

    upd4te login

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from '../screens/Home';
-import Login from '../screens/Login';
+import SignIn from '../screens/SignIn';
+import SignUp from '../screens/SignUp';
 
 function AppRouter() {
   return (
     <Router>
       <Routes>
-        <Route path="/home" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/" component={Home} />
+        <Route path="/SignIn" component={SignIn} />
+        <Route path="/SignUp" component={SignUp} />
+        <Route path="/" component={SignIn} />
       </Routes>
     </Router>
   );
